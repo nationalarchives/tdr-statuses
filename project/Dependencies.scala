49dab09eef34fabe2bc636b31942d17c7f822600
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-02-10 08:36:39 +0000

    Update rds, s3, ssm to 2.20.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   private val circeVersion = "0.14.3"
   private val testContainersVersion = "0.40.12"
-  private val awsVersion = "2.19.21"
+  private val awsVersion = "2.20.1"
   private val doobieVersion = "1.0.0-RC2"
 
   lazy val awsRds = "software.amazon.awssdk" % "rds" % awsVersion
