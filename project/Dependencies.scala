641e4ecc5d5085cf1aff7cdd9bd7283d360f4f37
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-03-19 08:32:56 +0000

    Update postgresql to 42.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
   lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
   lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
   lazy val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
-  lazy val postgres = "org.postgresql" % "postgresql" % "42.5.4"
+  lazy val postgres = "org.postgresql" % "postgresql" % "42.6.0"
   lazy val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.17.2"
   lazy val doobie = "org.tpolecat" %% "doobie-core" % doobieVersion
   lazy val doobiePostgres = "org.tpolecat" %% "doobie-postgres"  % doobieVersion
