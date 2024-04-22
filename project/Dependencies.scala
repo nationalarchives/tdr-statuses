26418eaf12257f72d77c13b344d2dcf0db7f271d
thanhz <thanhz@users.noreply.github.com>
2024-04-22 18:38:01 +0100

    Scala steward dependencies (#191)
    * Update scala-library to 2.13.13 in scala-steward-dependencies
    
    * Update sbt-assembly to 2.2.0 in scala-steward-dependencies
    
    * Update postgresql to 42.7.3 in scala-steward-dependencies
    
    * Update mockito-scala to 1.17.31 in scala-steward-dependencies
    
    * Update tdr-backend-checks-utils to 0.1.80 in scala-steward-dependencies
    
    * Update rds, s3, ssm to 2.25.35 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,17 +3,17 @@ import sbt._
 object Dependencies {
   private val circeVersion = "0.14.6"
   private val testContainersVersion = "0.41.3"
-  private val awsVersion = "2.24.10"
+  private val awsVersion = "2.25.35"
   private val doobieVersion = "1.0.0-RC5"
 
   lazy val awsRds = "software.amazon.awssdk" % "rds" % awsVersion
   lazy val awsSsm = "software.amazon.awssdk" % "ssm" % awsVersion
   lazy val awsS3 = "software.amazon.awssdk" % "s3" % awsVersion
-  lazy val backendCheckUtils = "uk.gov.nationalarchives" %% "tdr-backend-checks-utils" % "0.1.61"
+  lazy val backendCheckUtils = "uk.gov.nationalarchives" %% "tdr-backend-checks-utils" % "0.1.80"
   lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
   lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
   lazy val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
-  lazy val postgres = "org.postgresql" % "postgresql" % "42.7.2"
+  lazy val postgres = "org.postgresql" % "postgresql" % "42.7.3"
   lazy val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.17.6"
   lazy val doobie = "org.tpolecat" %% "doobie-core" % doobieVersion
   lazy val doobiePostgres = "org.tpolecat" %% "doobie-postgres"  % doobieVersion
@@ -21,6 +21,6 @@ object Dependencies {
   lazy val catsTesting = "org.typelevel" %% "cats-effect-testing-scalatest" % "1.5.0"
   lazy val testContainersScala = "com.dimafeng" %% "testcontainers-scala-scalatest" % testContainersVersion
   lazy val testContainersPostgres = "com.dimafeng" %% "testcontainers-scala-postgresql" % testContainersVersion
-  lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.30"
+  lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.31"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "3.0.1"
 }
