7394867fbef78f01756c25121294039cee38963f
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-07-04 08:30:20 +0000

    Update rds, s3, ssm to 2.26.15 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   private val circeVersion = "0.14.8"
 
   private val testContainersVersion = "0.41.4"
-  private val awsVersion = "2.26.12"
+  private val awsVersion = "2.26.15"
   private val doobieVersion = "1.0.0-RC5"
 
   lazy val awsRds = "software.amazon.awssdk" % "rds" % awsVersion
